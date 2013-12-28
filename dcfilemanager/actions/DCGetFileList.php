2ba59c800cd6b4c0803e39c6cf7c3fd1dd2b8400
philipp.sengelmann <p.sengelmann@bitforce-it.de>
2013-12-28 07:09:28 +0100

    dirty solution for file sorting problem (displaying order)

--- dcfilemanager/actions/DCGetFileList.php
+++ dcfilemanager/actions/DCGetFileList.php
@@ -6,7 +6,7 @@
 class DCGetFileList extends CAction {
 
     public function run($pathTofileListDirectory) {
-       
+
         if(!is_dir($pathTofileListDirectory ))
         {
             die(" Invalid Directory");
@@ -24,39 +24,39 @@ class DCGetFileList extends CAction {
         echo "[";
         foreach( new DirectoryIterator($pathTofileListDirectory) as $file) {
             $i++;
-            if ( $file->getBasename () !== '.DS_Store') {
-                if( $file->isFile() === TRUE && $file->getBasename() !=".htaccess" ) {
-                                
-                    echo "
+            if($file->isFile() === FALSE && $file->getBasename()!='.'&& $file->getBasename()!='..') {
+                echo "
                     {
                     \"attr\": { 
                         \"id\" : \"folder_".$i."\", 
                         \"path\" : \"".$pathTofileListDirectory."/".$file->getBasename()."\",
-                        \"rel\" : \"file\",
+                        \"rel\" : \"folder\",
                         \"page_id\" : \"".$i."\"
                         }, 
                     \"data\" : \"".$file->getBasename()."\",
                     \"csscl\":\"ui-state-error\",
-                    \"state\":\"empty\"
+                    \"state\":\"closed\"
                     },
                     ";
-                }else{  
-                    if($file->getBasename()!="."&&$file->getBasename()!=".." && $file->getBasename() !=".htaccess"){
-                        echo "
-                        {
-                        \"attr\": { 
-                            \"id\" : \"item_".$i."\", 
-                            \"path\" : \"".$pathTofileListDirectory."/".$file->getBasename()."\",
-                            \"rel\" : \"folder\",
-                            \"page_id\" : \"".$i."\" 
-                                }, 
-                        \"data\" : \"".$file->getBasename()."\",
-                        \"csscl\":\"ui-state-error\",
-                        \"state\":\"closed\"
-                        },
-                        ";
-                    }
-                }
+            }
+        }
+        $i=0;
+        foreach( new DirectoryIterator($pathTofileListDirectory) as $file) {
+            $i++;
+            if( $file->isFile() === TRUE ) {
+                echo "
+                     {
+                     \"attr\": { 
+                     \"id\" : \"file_".$i."\", 
+                     \"path\" : \"".$pathTofileListDirectory."/".$file->getBasename()."\",
+                     \"rel\" : \"file\",
+                     \"page_id\" : \"".$i."\" 
+                     }, 
+                     \"data\" : \"".$file->getBasename()."\",
+                     \"csscl\":\"ui-state-error\",
+                     \"state\":\"empty\"
+                     },
+                     ";
             }
         }
         /**
